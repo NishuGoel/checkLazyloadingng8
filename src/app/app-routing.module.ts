27918c93a4ee8646806eb7323c7778ba4a85ab1b
Nishu goel <nishugoe@in.ibm.com>
2020-01-01 17:45:32 +0530

    check lazy laod

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+// tslint:disable-next-line: max-line-length
+const routes: Routes = [{ path: 'loadfeaturemodule', loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule) },
+{
+  path: '',
+  redirectTo: '',
+  pathMatch: 'full'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
